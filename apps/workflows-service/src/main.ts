9539755e2c69c4113d21afebf2983b2bf5acd56e
Chibuzor Obiora <nbyodai@gmail.com>
2025-10-29 05:12:12 -0400

    add validation

--- apps/workflows-service/src/main.ts
+++ apps/workflows-service/src/main.ts
@@ -1,18 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { WorkflowsServiceModule } from './workflows-service.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // using NestFactory.create allows us to create hybrid apps that supports both http and nats entry points
   // useful when we want to expose http endpoints for health checks or metrics
   const app = await NestFactory.create(WorkflowsServiceModule);
+  app.useGlobalPipes(new ValidationPipe());
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.NATS,
-    options: {
-      servers: process.env.NATS_URL,
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: process.env.NATS_URL,
+      },
     },
-  });
+    {
+      // inherit global pipes from the http application
+      inheritAppConfig: true,
+    },
+  );
   await app.startAllMicroservices();
   await app.listen(3001);
 }
